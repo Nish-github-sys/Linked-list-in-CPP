461f68ab26dd5e6fb27edce14329d66fdb0b0632
Nishka Ranadive <nishka.amol.aug2006@gmail.com>
2025-09-25 12:22:57 +0530

    Add files via upload

---
+++ exp-17_1.cpp
@@ -0,0 +1,25 @@
+//Name: Nishka Ranadive
+//PRN: 24070123082
+//Exp-17
+
+//Program-1
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+
+    int val;
+    Node* next;
+
+    Node(int data){
+        val = data;
+        next = NULL;
+    }
+};
+
+int main(){
+    Node* n=new Node(20);
+    cout<<n->val<<" "<<n->next<<endl;
+    return 0;
+}
---
+++ exp-17_3.cpp
@@ -0,0 +1,8 @@
+//Name: Nishka Ranadive
+//PRN: 24070123082
+//Exp-17
+
+//Program-3
+#include<iostream>
+using namespace std;
+
